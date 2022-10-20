904db137067dc9646f539750ff4d1968e8d89a2f
Kravetsin <kravets.inside@gmail.com>
2022-10-20 20:02:44 +0200

    Buttons
    Кнопка чтобы скрыть или показать текст

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Counter from './Components/Counter';
 import CoolButton from './Components/First';
+import Comptest from './Components/Comptest';
 
 export default function App(){
   const [] = useState(0)
@@ -9,6 +10,7 @@ export default function App(){
     <div>
       <Counter/>
       <CoolButton/>
+      <Comptest name="Kravets"/>
     </div>
   )
 }
---
+++ src/Components/Comptest.jsx
@@ -0,0 +1,41 @@
+import { Component } from "react";
+
+
+export default class Comptest extends Component{
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         visibility: false
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            visibility: !state.visibility
+        }));
+    }
+
+
+    render(){
+        if (this.state.visibility){
+            return(
+                <div>
+                    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolor cum commodi quisquam ipsum temporibus amet porro quia corrupti quaerat natus dignissimos enim culpa labore, reprehenderit voluptas eius in? Quam!</h1>
+                    <div className="col">
+                        <button onClick={this.handleClick}>Close</button>
+                    </div>
+                </div>
+            );
+        }
+        else{
+            return(
+                <div className="col">
+                    <button onClick={this.handleClick}>Open</button>
+                </div>
+            );
+        }
+    }
+}
+Comptest.defaultProps = {name: "Ihor"}
